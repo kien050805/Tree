fd2f50ef181349ec689919d86aaefaa9b0791cfd
Bach Nguyen <nguyen_b3@denison.edu>
2024-11-03 18:09:16 -0500

    BST header initial

---
+++ BST.hpp
@@ -0,0 +1,52 @@
+/*===========================================================================
+Kien Le, Nguyen Nguyen, Bach Nguyen
+7 November 2024
+BST.hpp
+This file contains the definitions of the binary search tree implementations
+===========================================================================*/
+
+#include <iostream>
+using namespace std;
+
+#include "BSTNode.hpp"
+
+#ifndef BST
+#define BST
+
+template <class T> 
+class BST
+{
+	private:
+		// struct for Node for Binary Search Tree
+		struct Node
+		{
+			T		key;
+            Node    *p;
+			Node 	*left;
+            Node    *right;
+		};
+		
+		long		size;	
+
+	public:
+					BST		                    ( void );
+					BST		                    ( const BST<T> &tree );
+					~BST		                ( void );
+		BST<T>		operator=	                ( const BST<T> &tree );
+
+        void        transplant                  ( BSTNode<T> *oldNode, BSTNode<T> *newNode);
+		bool        isEmpty()                   const;
+        long        size()                      const;
+        BSTNode<T>* insert                      (T value);
+        void        remove                      (T value);
+        BSTNode<T>* search                      (T value) const;
+        BSTNode<T>* treeMin()                   const;
+        BSTNode<T>* treeMax()                   const;
+        void        printPreOrderTraversal()    const;
+        void        printInOrderTraversal()     const;
+        void        printPostOrderTraversal()   const;
+
+};
+
+#include "BST.cpp"
+#endif
