c04b358a0d4385fb1f7b27717de0c37182c0c27f
MaxTBM <animalsfreeworld@gmail.com>
2024-11-03 18:16:17 -0500

    pushed

---
+++ BSTNode.hpp
@@ -0,0 +1,45 @@
+/*===========================================================================
+Kien Le, Nguyen Nguyen, Bach Nguyen
+12 October 2024
+BSTNode.hpp
+This file contains the definitions of the sorting functions.
+===========================================================================*/
+
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+#ifndef BSTNODE
+#define BSTNODE
+
+template <class T>
+class BSTNode
+{
+    private:
+        struct Node
+        {
+            T key;
+            Node *P;
+            Node *left;
+            Node *right;
+        };
+        long size;
+
+        void copy(const BSTNode<T> &node);
+
+
+    public:
+                        BSTNode                 (void);
+                        BSTNode                 (const BSTNode<T> &node);
+                        ~BSTNode                (void);
+        BSTNode<T>*     treeMin                 ();
+        BSTNode<T>*     treeMax                 ();
+        void            printPreOrderTraversal  () const;
+        void            printInOrderTraversal   () const;
+        void            printPostOrderTraversal () const;
+        BSTNode<T>*		operator=	            (const BSTNode<T> &node);
+};
+
+#include "BSTNode.cpp"
+#endif
