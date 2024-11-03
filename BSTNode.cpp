721698b1259371d4494ae5a983b5e33b02a61608
kien050805 <143416413+kien050805@users.noreply.github.com>
2024-11-03 18:02:47 -0500

    bstnode

---
+++ BSTNode.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include "BSTNode.hpp"
+
+using namespace std;
+
+template <class T>
+BSTNode<T>:: BSTNode(void)
+{};
+
+template <class T>
+BSTNode<T>:: List(const BSTNode<T> &BSTNode)
+{};
+
+template <class T>
+BSTNode<T>:: ~BSTNode(void)
+{};
+
+template <class T>
+BSTNode<T>:: operator= (const BSTNode<T>)
+{};
+
+template <class T>
+BSTNode<T>* BSTNode<T>:: treeMin() const
+{};
+
+template <class T>
+BSTNode<T>*  BSTNode<T>:: treeMax() const
+{};
---
+++ RBTreeNode.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+using namespace std;
+
+#ifndef RBTREENODE_HPP
+#define RBTREENODE_HPP
+
+template <class T>
+class RBTreeNode {};
