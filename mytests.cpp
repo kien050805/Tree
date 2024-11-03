ca8307366c15af4ff769945d09f83fbc99f84652
MaxTBM <animalsfreeworld@gmail.com>
2024-11-03 18:40:03 -0500

    test_functions

--- mytests.cpp
+++ mytests.cpp
@@ -8,6 +8,67 @@
 #include "RBTree.hpp"
 #include "RBTree.cpp"
 
+/*===========================================================================
+BSTNode_sanity_check()
+Run a series of predefined test cases to ensure that the BSTNode class is 
+functioning correctly.
+Parameters: None
+Return: boolean value depending on if the function pass the test cases
+===========================================================================*/
+bool BSTNode_sanity_check()
+{
+    bool check = true;
+    
+    
+    return check;
+};
+
+/*===========================================================================
+BST_sanity_check()
+Run a series of predefined test cases to ensure that the BST class is 
+functioning correctly.
+Parameters: None
+Return: boolean value depending on if the function pass the test cases
+===========================================================================*/
+bool BST_sanity_check()
+{
+    bool check = true;
+
+    
+    return check;
+};
+
+/*===========================================================================
+RBTNode_sanity_check()
+Run a series of predefined test cases to ensure that the RBTNode class is 
+functioning correctly.
+Parameters: None
+Return: boolean value depending on if the function pass the test cases
+===========================================================================*/
+bool RBTNode_sanity_check()
+{
+    bool check = true;
+
+    
+    return check;
+};
+
+/*===========================================================================
+RBT_sanity_check()
+Run a series of predefined test cases to ensure that the RBT class is 
+functioning correctly.
+Parameters: None
+Return: boolean value depending on if the function pass the test cases
+===========================================================================*/
+bool RBT_sanity_check()
+{
+    bool check = true;
+
+    
+    return check;
+};
+
+
 int main()
 {
 
