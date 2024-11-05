5282af91677d090ad92a142c027bfcd43a1484cb
kien050805 <143416413+kien050805@users.noreply.github.com>
2024-11-05 08:48:55 -0500

    fix+

--- BST.hpp
+++ BST.hpp
@@ -30,16 +30,16 @@ class BST
 		BST<T>			operator=	                ( const BST<T> &tree );
 
         void        	transplant                  ( BSTNode<T> *oldNode, BSTNode<T> *newNode);
-		bool        	isEmpty()                   const;
-        long        	size()                      const;
+		bool        	isEmpty                     () const;
+        long        	size                        () const;
         BSTNode<T>* 	insert                      (T value);
         void        	remove                      (T value);
         BSTNode<T>* 	search                      (T value) const;
-        BSTNode<T>* 	treeMin()                   const;
-        BSTNode<T>* 	treeMax()                   const;
-        void        	printPreOrderTraversal()    const;
-        void        	printInOrderTraversal()     const;
-        void        	printPostOrderTraversal()   const;
+        BSTNode<T>* 	treeMin                     () const;
+        BSTNode<T>* 	treeMax                     () const;
+        void        	printPreOrderTraversal      () const;
+        void        	printInOrderTraversal       () const;
+        void        	printPostOrderTraversal     () const;
 
 };
 
