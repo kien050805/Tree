c4c541eeda5c47321788ef02cc53b60ac633d4a9
kien050805 <143416413+kien050805@users.noreply.github.com>
2024-11-05 08:52:06 -0500

    exceptions

--- customexceptions.hpp
+++ customexceptions.hpp
@@ -5,46 +5,18 @@ using namespace std;
 #ifndef CUSTOMEXCEPTIONS_HPP
 #define CUSTOMEXCEPTIONS_HPP
 
-class value_not_in_tree_exception : public std::exception {
+class value_not_in_tree_exception : public exception {
 public:
-    const char* what() const noexcept override {
+    const char* what() const throw() {
         return "Error: The specified value is not in the tree.";
     }
 };
 
-class empty_tree_exception : public std::exception {
+class empty_tree_exception : public exception {
 public:
-    // Override the what() function to provide an error message
-    const char* what() const noexcept override {
+    const char* what() const throw() {
         return "Error: This is an empty tree.";
     }
-};
-
-template <class T> 
-class BST
-{
-	private:
-		// struct for Node for Binary Search Tree
-		struct Node
-		{
-			T		key;
-            Node    *p;
-			Node 	*left;
-            Node    *right;
-		};
-		Node		*root;
-		long		size;	
-
-	public:
-        bool isEmpty() const {
-            return size == 0;
-        }
-
-        void checkEmpty() {
-            if (isEmpty()) {
-                throw empty_tree_exception();
-            }
-        }
 };
 
 #endif
